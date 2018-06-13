0c62562a66a895d1c451cbc45b9f1d18014410b8
LAPTOP-JOH2LUUE\Ctrl Pay Aluno 203 <l.alvesctrlplay@gmail.com>
2018-06-13 14:18:06 -0300

    [ADD] cena 4 e script da cena 4

---
+++ TestProject/Assets/ScriptCena4.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCena4 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Script da cena 4 =D");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
