40f7330398d66cc116e14ec17ab1b7faadc7be80
LAPTOP-JOH2LUUE\Ctrl Pay Aluno 203 <l.alvesctrlplay@gmail.com>
2018-06-13 14:44:01 -0300

    script para testar conflito

---
+++ TestProject/Assets/conflito.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conflito : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Não quero manter isso");
+		Debug.Log ("Mas quero manter esse aqui!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
