0af59c6311417da327f4c419e1606fbd742cac0d
Professor Sala 2 <d.oliveira@ctrlplay.com.br>
2018-06-13 14:23:04 -0300

    Cena 3 e Script de Movimento

---
+++ TestProject/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+
+		speed = 1;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+
+			this.transform.Translate (Vector3.forward);
+		}
+		
+	}
+}
